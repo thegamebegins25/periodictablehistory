6fb9a7b817afb338af4ab91544d866fa74716663
thegamebegins25 <wdppostcard@gmail.com>
2022-04-20 07:05:16 -0400

    Revert "#"
    This reverts commit 53a063f72658796435b60973c4dd019ea06f0487.

--- main.js
+++ main.js
@@ -1,14 +1,6 @@
 var coll = document.getElementsByClassName("collapsible");
 var i;
 
-document.onload = function load() { 
-  const urlSearchParams = new URLSearchParams(window.location.search);
-  const params = Object.fromEntries(urlSearchParams.entries());
-  let scrollto = params['scrollto'];
-  console.log(scrollto);
-  document.getElementById(scrollto).scrollIntoView();
-}
-
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
@@ -23,9 +15,4 @@ for (i = 0; i < coll.length; i++) {
 
 function docs() {
   window.open("https://docs.google.com/document/d/1Gz4PHn0TTUuLnntRtjEfsKWWBFz76eB7RxVyjDH57nQ/edit?usp=sharing");
-}
-
-function link(id) {
-  let text = "https://thegamebegins25.github.io/periodictablehistory#" + id
-  navigator.clipboard.writeText(text);
 }
